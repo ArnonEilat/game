cc06a67543775dcb8c958a01c6f3f9f745c96e52
Alon Gal <alon.gal@khealth.ai>
2021-01-15 22:07:36 +0200

    Basic game logic

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -1,18 +1,57 @@
 import React from "react";
+import { Guess } from './types'
 
-const TicTacToeBoard = (props: any) => (
-  <div>
-    <h1>Player {props.ctx.currentPlayer} is drawing!</h1>
-    <ul>
-      {props.G.guesses.map(
-        (guess: { guess: string; guesser: string; correct: boolean }, i: number) => (
-          <li
-            key={`guess${i}`}
-          >{`Player ${guess.guesser} guessed ${guess.correct ? 'correctly!' : guess.guess}`}</li>
-        )
+const PlayerInput = (props: any) => (
+  <input type="text" onKeyPress={props.onKeyPress} />
+);
+
+export const DrawPhase = (props: any) => {
+  const isCurrentPlayer = props.ctx.currentPlayer === props.playerID;
+
+  return (
+    <div>
+      {isCurrentPlayer ? <h2>Your turn to draw!</h2> : <h2>Player {props.ctx.currentPlayer} is drawing!</h2>}
+      {!isCurrentPlayer && (
+        <div>
+          Type your guess:
+          <PlayerInput
+            onKeyPress={(e: any) => {
+              if (e.key === "Enter") {
+                props.moves.guess(e.target.value);
+              }
+            }}
+          />
+        </div>
       )}
-    </ul>
+      {isCurrentPlayer && <div>Secret word is {props.G.correctWord}</div>}
+      <ul>
+        {props.G.guesses.map(
+          (
+            guess: Guess,
+            i: number
+          ) => (
+            <li key={`guess${i}`}>{`Player ${guess.guesser} guessed ${
+              guess.correct ? "correctly!" : guess.guess
+            }`}</li>
+          )
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export const AnnounceWinnerPhase = (props: any) => (
+  <div>
+    <h1>The grand and glorious winner is player {props.G.winner}</h1>
   </div>
 );
 
-export { TicTacToeBoard };
+export const SketchBoard = (props: any) => (
+  <div style={{ height: "250px", border: "2px solid black" }}>
+    {props.ctx.phase === "draw" ? (
+      <DrawPhase {...props} />
+    ) : (
+      <AnnounceWinnerPhase {...props} />
+    )}
+  </div>
+);
---
+++ src/game/types.ts
@@ -0,0 +1,5 @@
+export type Guess = {
+  guess: string,
+  guesser: string,
+  correct: boolean
+}
