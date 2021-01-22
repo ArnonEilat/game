6282c3a86d2ecd4ecc492e7d1ed7e6cbf134c8ea
ArnonEilat <ArnonEilat@users.noreply.github.com>
2021-01-22 14:24:46 +0000

    Prettified Code!

--- src/game/draw-stage/draw-stage.tsx
+++ src/game/draw-stage/draw-stage.tsx
@@ -5,13 +5,13 @@ import { BoardPropTypes } from '../types';
 
 export const DrawStage = (props: BoardPropTypes) => {
   return (
-    <div className='container'>
-      <div className='left'>
+    <div className="container">
+      <div className="left">
         <h2>Your turn to draw!</h2>
         <div>Secret word is {props.G.correctWord}</div>
         <GuessList guesses={props.G.guesses} />
       </div>
-      <div className='right'>
+      <div className="right">
         <Painter moves={props.moves} />
       </div>
     </div>
